94aaa59fea09362e0945b962b185e23cd52c3864
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-07-23 15:54:45 -0500

    feat: in3050 method

--- src/codingBat.java
+++ src/codingBat.java
@@ -289,6 +289,16 @@ public class codingBat {
         return false;
     }
 
+    public static boolean in3050(int a, int b) {
+  if(a>=30 && a <=40 && b>=30 && b<=40){
+      return true;
+  }else if(a>=40 && a <=50 && b>=40 && b<=50){
+      return true;
+  }else{
+      return false;
+  }
+    }
+
     public static int divide(int a,int b){
         return a / b;
     }
