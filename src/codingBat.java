0a2ed872e918b75351be2139fb99402789d6c765
Kelvon Patterson <kelvonpatterson044@gmail.com>
2021-07-25 23:38:33 -0500

    feat: last2 Method

--- src/codingBat.java
+++ src/codingBat.java
@@ -342,6 +342,24 @@ public class codingBat {
         return result.toString();
     }
 
+    public int last2(String str) {
+
+        if (str.length() < 2) return 0;
+
+        String end = str.substring(str.length()-2);
+
+        int count = 0;
+
+
+        for (int i=0; i<str.length()-2; i++) {
+            String sub = str.substring(i, i+2);
+            if (sub.equals(end)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
     public static int divide(int a,int b){
         return a / b;
     }
